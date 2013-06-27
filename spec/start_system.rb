bb8688dcfc3fd9821e96afa4dc9329f44d80d618
vikas <vikasyaligar.it@gmail.com>
2013-06-27 23:56:30 +0530

    Simple spec to check if sinatra app is runing or not

--- spec/start_system.rb
+++ spec/start_system.rb
@@ -1,6 +1,12 @@
 require 'rspec'
 require '../start.rb'
 
+#Please do gem install rack && gem install rack-test before running the test
+
+require 'rack/test'
+
+set :environment , :test
+
 describe "class Server" do
 
   it "should check if database exists" do
@@ -13,4 +19,11 @@ describe "class Server" do
     server.start_crawling
     server.crawling_status.should be_true
   end
+
+  it "should start sinatra serve" do
+    server = Server.new('test.db')
+    get '/'
+    last_response.should be_ok
+  end
+
 end
--- start.rb
+++ start.rb
@@ -1,3 +1,4 @@
+require 'sinatra'
 class Server
   #your code here
   #
