c9def0418d92eef113780af14133c3ed2e8ebfe7
Mitesh Mutha <mtshmtha@gmail.com>
2013-06-28 22:28:58 +0530

    Passed test 1....

--- start.rb
+++ start.rb
@@ -1,13 +1,25 @@
+require 'active_record'
 require 'sinatra'
+
+class Word < ActiveRecord::Base
+  establish_connection adapter: 'sqlite3', database: 'test.db'
+  connection.create_table table_name, force: true do |t|
+    t.string :word
+    t.text :links
+  end
+end
+
 class Server
-  #your code here
-  #
-  def intialize
+  def initialize table
 
   end
 
   def check_table table
-
+    if (defined?table)
+      return true
+    else
+      return false
+    end
   end
 
   def start_crawling
@@ -17,5 +29,5 @@ class Server
   def crawling_status
 
   end
-
 end
+
