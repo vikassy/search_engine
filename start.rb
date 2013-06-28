c98a8ce5b1c52b3ad56f1736628812105d365600
Karthik Senthil <karthik.senthil94@gmail.com>
2013-06-29 00:53:43 +0530

    Final code for test 3

--- start.rb
+++ start.rb
@@ -4,13 +4,6 @@ require 'sinatra'
 get '/' do
 end
 
-#class Word < ActiveRecord::Base
-#  establish_connection adapter: 'sqlite3', database: 'test.db'
-#  connection.create_table table_name, force: true do |t|
-#    t.string "word"
-#    t.string "links"
-#  end
-#end
 class Server
   #your code here
   #
